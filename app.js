a77875c8edd280c401f992438c1127f33f6c0fd7
Volikaris <venomkvster@gmail.com>
2016-09-25 20:57:36 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var mongojs = require("mongojs");
-var db = mongojs('localhost:27017/myGame', ['acc','progress']);
+//var mongojs = require("mongojs");
+var db = null;//mongojs('localhost:27017/myGame', ['acc','progress']);
 
 var express = require('express');
 var app = express();
@@ -10,7 +10,7 @@ app.get('/',function(req, res) {
 });
 app.use('/client',express.static(__dirname + '/client'));
 
-serv.listen(2000);
+serv.listen(process.env.PORT || 2000);
 console.log("Server started.");
 
 var SOCKET_LIST = {};
@@ -230,25 +230,28 @@ Bullet.getAllInitPack = function(){
 var DEBUG = true;
 
 var isValidPassword = function(data,cb){
-	db.acc.find({username:data.username,password:data.password},function(err,res){
+	return cb(true);
+	/*db.acc.find({username:data.username,password:data.password},function(err,res){
 		if(res.length > 0)
 			cb(true);
 		else
 			cb(false);
-	});
+	});*/
 }
 var isUsernameTaken = function(data,cb){
-	db.acc.find({username:data.username},function(err,res){
+	return cb(false);
+	/*db.acc.find({username:data.username},function(err,res){
 		if(res.length > 0)
 			cb(true);
 		else
 			cb(false);
-	});
+	});*/
 }
 var addUser = function(data,cb){
-	db.acc.insert({username:data.username,password:data.password},function(err){
+	return cb();
+	/*db.acc.insert({username:data.username,password:data.password},function(err){
 		cb();
-	});
+	});*/
 }
 
 var io = require('socket.io')(serv,{});
